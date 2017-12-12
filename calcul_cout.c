8fdfa5035254859e22b5b05fd7c65a59faf48b56
Far4way <florianlaffont09300@gmail.com>
2017-12-12 10:09:31 +0100

    Add files via upload

---
+++ calcul_cout.c
@@ -0,0 +1,49 @@
+#include<stdlib.h>
+#include<stdio.h>
+/*
+Création de la fonction créant le tableau 1D cout_final et le tableau 2D pere.
+
+*/
+void calcul_cout(unsigned int* cout_final, unsigned char** energie, unsigned int** pere,int nl,int nc){
+
+    unsigned int** cout;
+    cout_final=calloc(nc,sizeof(*cout_final));
+
+    cout = alloue_image_int(nl,nc);
+    pere = alloue_image_int(nl,nc);
+    int i,j,k;
+
+
+
+    for(i=0;i<nl;i++){
+        if(i==0){
+            for(j=0;j<nc;j++){
+                cout[i][j]=0;
+                pere[i][j]=-1;
+            }
+        }
+        else{
+            for(j=0;j<nc;j++){
+                pere[i][j]=0;
+                cout[i][j]=-1;
+            }
+        }
+
+
+    }
+
+
+
+    for(i=1;i<nl;i++){
+        for(j=0;j<nc;j++){
+            for(k=-1;k<2;k++){
+                if( (j+k>=0) && (j+k<nc) && (cout[i][j]>(cout[i-1][j+k]+energie[i-1][j+k]))){
+                    cout[i][j]=cout[i-1][j+k]+energie[i-1][j+k];
+                    pere[i][j]=j+k;               }
+            }
+        }
+
+
+    }
+
+}
---
+++ energie.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+/*
+Création de la fonction calculant l'énergie / gradient de l'image d'entrée, à traiter.
+
+*/
+
+unsigned char** gradienty(unsigned char** energ, unsigned char** im, int nl, int nc){
+    if (energ==NULL){
+    energ=alloue_image_char(nl,nc);
+    }
+    int  i,j;
+    for ( i=0;i<nl;i++){
+        for( j=0;j<nc;j++){
+            energ[i][j]= abs(im[i][(j-1+nc)%nc]-im[i][(j+1)%nc]);
+        }
+
+    }
+
+    return energ;
+}
