a3f9019a398e4ed6106ed50224356ff0617631ed
Christian Chen <34471323+chenchristian06@users.noreply.github.com>
2017-12-17 12:56:40 +0100

    Update trouve_chemin.c

--- trouve_chemin.c
+++ trouve_chemin.c
@@ -13,5 +13,20 @@
 #include "projet.h"
 
 void trouve_chemin(unsigned int* chemin, unsigned int** pere, unsigned int* cout_final, int nl, int nc){
+  int ind_moinscouteuse = 0; /*on initialise l'indice la moins couteuse au premeir élément*/
+  int i = 1; /* indice de parcours*/
+  unsigned int* min = cout_final[nl-1][0];
+  for(;i<nc;i++){/*on va parcourir élément par élément dans la dernière ligne */
+    if(min>cout_final[nl-1][i]){
+      min = cout_final[nl-1][i];
+      ind_moinscouteuse = i;
+    }
+  }
+  chemin[nl-1] = ind_moinscouteuse;
+  int k;
+  for(k=nl-2:k>-1;k--){
+    chemin[k] = pere[k+1][chemin[k+1]];
+  }
+  
 
 }
